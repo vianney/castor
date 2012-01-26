a7afca4d2ddcda5a53805181451dce1cb8b153d4
Vianney le Clément de Saint-Marcq <vleclement@gmail.com>
2012-01-26 15:37:55 +0100

    Switch license to GPLv3 and update headers for 2012

--- tools/castor.cpp
+++ tools/castor.cpp
@@ -1,21 +1,20 @@
 /* This file is part of Castor
  *
  * Author: Vianney le Clément de Saint-Marcq <vianney.leclement@uclouvain.be>
- * Copyright (C) 2010-2011, Université catholique de Louvain
+ * Copyright (C) 2010-2012, Université catholique de Louvain
  *
- * Castor is free software; you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; version 2 of the License.
+ * the Free Software Foundation, version 3 of the License.
  *
- * Castor is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with Castor; if not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
 #include <iostream>
 #include <iomanip>
 #include <fstream>
